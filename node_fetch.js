a34e8df91f333c0d3c506df4081184b300d46bb5
Rowan Cannaday <cannadayr@gmail.com>
2018-10-02 13:48:42 -0500

    import querystring lib and simple use-case

--- node_fetch.js
+++ node_fetch.js
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
+// import libs
 const fetch = require('node-fetch');
+const querystring = require('querystring');
 
+// api specific vars
+const base_url = "https://ss6b2ke2ca.execute-api.us-east-1.amazonaws.com/Prod/quotes?";
+
+var params = {
+    a: 1,
+    b: 2,
+    c: 3
+};
+
+let queryString = querystring.stringify(params);
+
+console.log(queryString);
+/*
 fetch('https://api.github.com/users/github')
     .then(res => res.json())
     .then(json => console.log(json));
+*/
 
