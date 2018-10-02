437f641c75a46b536c7b3853d500c19753b93c43
Rowan Cannaday <cannadayr@gmail.com>
2018-10-02 13:59:46 -0500

    successful query of api path
    change ignore path of env file

--- node_fetch.js
+++ node_fetch.js
@@ -2,22 +2,25 @@
 // import libs
 const fetch = require('node-fetch');
 const querystring = require('querystring');
+require('dotenv').config()
 
 // api specific vars
 const base_url = "https://ss6b2ke2ca.execute-api.us-east-1.amazonaws.com/Prod/quotes?";
 
 var params = {
-    a: 1,
-    b: 2,
-    c: 3
+    creditScore: 700,
+    propertyType: "SingleFamily",
+    occupancy: "Primary",
+    loanSize: 450000
 };
 
-let queryString = querystring.stringify(params);
+let query_string = querystring.stringify(params);
 
-console.log(queryString);
-/*
-fetch('https://api.github.com/users/github')
+console.log(query_string);
+
+fetch(base_url + query_string,{
+        headers: { 'Authorization': 'RG-AUTH ' + process.env.RG_AUTH }
+    })
     .then(res => res.json())
     .then(json => console.log(json));
-*/
 
