7e379504c4ff48b3d2504f4805c18e3ae3657121
Rowan Cannaday <cannadayr@gmail.com>
2018-10-02 13:21:19 -0500

    add loan size, don't echo url, echo newline
    receiving results from aws

--- curl.sh
+++ curl.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 . env.sh
 BASE_URL="https://ss6b2ke2ca.execute-api.us-east-1.amazonaws.com/Prod/quotes?"
-CREDIT="&creditScore=300"
+CREDIT="&creditScore=700"
 PROPERTY="&propertyType=SingleFamily"
 OCCUPANCY="&occupancy=Primary"
-PARAMS="${CREDIT}${PROPERTY}${OCCUPANCY}"
+LOAN="&loanSize=450000"
+PARAMS="${CREDIT}${PROPERTY}${OCCUPANCY}${LOAN}"
 URL="${BASE_URL}${PARAMS}"
-echo "url = '${URL}'"
+#echo "url = '${URL}'"
 curl --header "Authorization: RG-AUTH ${RG_AUTH}" ${URL}
+echo
 
