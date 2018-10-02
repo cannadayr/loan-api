53af0a2b9275147a33290f144970f3a5c78cc23a
Rowan Cannaday <cannadayr@gmail.com>
2018-10-02 13:30:46 -0500

    rm loantype as get arg (cant query-by)
    loanType is only a field on output object
    can't include in getvars

--- curl.sh
+++ curl.sh
@@ -5,10 +5,10 @@ CREDIT="&creditScore=700"
 PROPERTY="&propertyType=SingleFamily"
 OCCUPANCY="&occupancy=Primary"
 LOAN="&loanSize=450000"
-TYPE="&loanType=30YR%20Fixed"
-PARAMS="${CREDIT}${PROPERTY}${OCCUPANCY}${LOAN}${TYPE}"
+#TYPE="&loanType=30YR%20Fixed"
+PARAMS="${CREDIT}${PROPERTY}${OCCUPANCY}${LOAN}"
 URL="${BASE_URL}${PARAMS}"
-echo "url = '${URL}'"
+#echo "url = '${URL}'"
 curl --header "Authorization: RG-AUTH ${RG_AUTH}" ${URL}
 echo
 
