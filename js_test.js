54f79d0f5717aebef510805cc8358fd0bed29630
Rowan Cannaday <cannadayr@gmail.com>
2018-10-06 15:43:52 -0500

    rm old test script, run comparison in js now

--- js_test.js
+++ js_test.js
@@ -10,10 +10,20 @@ const quotes = require('./test_data/api_output.json');
 // import lib
 const lib = require('./src/lib.js');
 
+// set expected result
+expectedRate = 4.125;
+
 // get length of cust data
 var custDataLen = data.length;
 
 var result = lib.handleReq(data,quotes);
 
-console.log(result);
+lowestRate = result.lowestQuote.interestRate;
+
+if (lowestRate == expectedRate) {
+    console.log('the test passed!');
+}
+else {
+    console.log('the test failed, lowestRate != expectedRate');
+}
 
--- run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-test "$(./js_test.js | xargs)" = "{ loanSize: 450000, creditScore: 700, propertyType: SingleFamily, occupancy: Primary, lowestQuote: { lenderName: Online National Financial Institution, loanType: 30YR Fixed, interestRate: 4.125, closingCosts: 11250, monthlyPayment: 2180.9237962410334, apr: 4.332233224568883 } }" \
-&& echo "it passes!"
-
-
